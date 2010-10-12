e230f3ab4b4b98aa894f4e71844fc070c30d2ca3
Shad Sharma <shads@wolfram.com>
2010-10-12 14:53:19 -0500

    added builtin symbols

--- Support/tools/language_grammar.py
+++ Support/tools/language_grammar.py
@@ -5,32 +5,23 @@ def to_camelcase(function):
     return re.sub('(((?<=[a-z])[A-Z])|([A-Z](?![A-Z]|$)))', '_\\1', function).lower().strip('_')
 
 def print_function_grammar(functions):
-    result = []
-
-    for function in functions:
-        result.append("""
-                {    name = 'support.function.mathematica.system.%s';
-                     match = '\\b(%s)\\b';
-                },""" % (to_camelcase(function), function))
+    print """
+        {   name = 'support.function.mathematica.system';
+            match = '\\b(%s)\\b';
+        },""" % "|".join(functions)
     
-    print "".join(result)
 
 def print_symbol_grammar(symbols):
-    result = []
-    
-    for symbol in symbols:
-        symbol = symbol.strip("$")
-        result.append("""
-                {   name = 'support.variable.mathematica.system.%s';
-                    match = '(\\$%s)\\b';
-                },""" % (to_camelcase(symbol), symbol))
-    
-    print "".join(result)
+    print """
+        {   name = 'support.variable.mathematica.system';
+            match = '(\\%s)\\b';
+        },""" % "|".join(symbols)
 
 def main():
     fp = open('symbols.json', 'r')
     functions, symbols = eval(fp.read())
     
+    # print_function_grammar(functions)
     print_symbol_grammar(symbols)
 
 if __name__ == '__main__':
